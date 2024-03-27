12b32c2896963923f77b0cd3aabb2f03fe3c23f1
SALES <5235127+adriens@users.noreply.github.com>
2024-03-27 15:15:34 +1100

    Update duck.sql
    cf #3

--- duck.sql
+++ duck.sql
@@ -3,9 +3,8 @@ create
 or replace table domaine (
     domain VARCHAR NOT NULL,
     marque_nom  VARCHAR UNIQUE,-- NULL quand pas de marque associée (domaine réservé)
-    marque_description VARCHAR NOT NULL,
+    marque_description VARCHAR,-- NULL quand pas de marque associée (domaine réservé)
     url VARCHAR
-
 );
 
 insert into
